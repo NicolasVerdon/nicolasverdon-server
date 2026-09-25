073a77dab4a9a6b17caccc0588ceafffd2ebc2b9
Nicolas Verdon <verdon.nicolas@gmail.com>
2026-09-25 08:46:10 +0200

    Update sport title and manifest

--- main.py
+++ main.py
@@ -290,6 +290,7 @@ ui.run_with(
     app,
     mount_path="/sport",
     storage_secret=os.environ["NICEGUI_STORAGE_SECRET"],
+    title="Sport Dashboard",
     favicon="static/sport-icon.png",
     )
 
